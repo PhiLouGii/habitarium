f264ecf118d7443ce94c225b1069a43d26d8b8e8
Philippa Giibwa <p.giibwa@alustudent.com>
2025-07-02 20:59:26 +0200

    guest mode

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import { useGuest } from '../context/GuestContext';
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { user, logout } = useAuth(); // Add logout to the destructuring
   const navigate = useNavigate();
+  const { isGuest } = useGuest();
 
   // If there's no user, redirect to login
-  if (!user) {
+ if (!user && !isGuest) {
     return <Navigate to="/login" replace />;
   }
 
