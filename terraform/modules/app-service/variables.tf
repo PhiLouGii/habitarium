e9edc4005c6f3b03770ce84dc9d5bde929a4666d
Philippa Giibwa <p.giibwa@alustudent.com>
2025-07-21 00:43:54 +0200

    Update app service variables for habit tracker deployment

---
+++ terraform/modules/app-service/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "ecr_repository_url" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
