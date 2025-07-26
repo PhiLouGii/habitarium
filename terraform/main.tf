160cbacb9cdbc3eea9acadff470296a24e53b5e5
Philippa Louise Giibwa <125802100+PhiLouGii@users.noreply.github.com>
2025-07-26 02:44:13 +0200

    Update main.tf with revised infrastructure configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,145 +1,186 @@
 terraform {
   required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
 }
 
-provider "google" {
-  project = var.project_id
-  region  = var.region
+provider "azurerm" {
+  features {}
 }
 
-# Enable required APIs
-resource "google_project_service" "required_apis" {
-  for_each = toset([
-    "run.googleapis.com",
-    "sql-component.googleapis.com",
-    "sqladmin.googleapis.com",
-    "containerregistry.googleapis.com",
-    "artifactregistry.googleapis.com"
-  ])
-  
-  project = var.project_id
-  service = each.value
-  
-  disable_on_destroy = false
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
 }
 
-# Create Artifact Registry repository
-resource "google_artifact_registry_repository" "app_repo" {
-  repository_id = "${var.app_name}-repo"
-  location      = var.region
-  format        = "DOCKER"
-  description   = "Docker repository for ${var.app_name}"
-  
-  depends_on = [google_project_service.required_apis]
+variable "resource_group_name" {
+  type        = string
+  description = "Azure Resource Group name"
 }
 
-# Create Cloud SQL instance
-resource "google_sql_database_instance" "main" {
-  name             = "${var.app_name}-db-instance"
-  database_version = "POSTGRES_15"
-  region           = var.region
-  
-  settings {
-    tier = "db-f1-micro"
+variable "location" {
+  type        = string
+  description = "Azure region, e.g. eastus, centralus"
+}
+
+variable "app_name" {
+  type        = string
+  description = "App name prefix, used for Azure resource names"
+}
+
+variable "db_username" {
+  type        = string
+  description = "PostgreSQL admin username"
+  default     = "pgadmin"
+}
+
+variable "db_password" {
+  type        = string
+  description = "PostgreSQL admin password"
+  sensitive   = true
+}
+
+variable "acr_admin_enabled" {
+  type        = bool
+  description = "Enable admin user on the Azure Container Registry"
+  default     = true
+}
+
+# Ensure the resource group exists
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Enable resource providers (usually not needed if you use portal or CLI, but explicit here)
+resource "azurerm_resource_provider_registration" "web" {
+  provider_namespace = "Microsoft.Web"
+  depends_on         = [azurerm_resource_group.rg]
+}
+
+resource "azurerm_resource_provider_registration" "acr" {
+  provider_namespace = "Microsoft.ContainerRegistry"
+  depends_on         = [azurerm_resource_group.rg]
+}
+
+resource "azurerm_resource_provider_registration" "postgres" {
+  provider_namespace = "Microsoft.DBforPostgreSQL"
+  depends_on         = [azurerm_resource_group.rg]
+}
+
+# Create Azure Container Registry for Docker images
+resource "azurerm_container_registry" "acr" {
+  name                = "${var.app_name}acr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = var.acr_admin_enabled
+}
+
+# Create Azure Database for PostgreSQL server
+resource "azurerm_postgresql_server" "postgres_server" {
+  name                = "${var.app_name}-psql-server"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  administrator_login          = var.db_username
+  administrator_login_password = var.db_password
+  version                     = "15" # PostgreSQL version 15
+  sku_name                    = "B_Gen5_1" # Basic tier with 1 vCore
+
+  storage_mb = 5120
+  backup_retention_days = 7
+  geo_redundant_backup_enabled = false
+  auto_grow_enabled = true
+  public_network_access_enabled = true
+
+  ssl_enforcement_enabled = true
+}
+
+# Create a database in the PostgreSQL server
+resource "azurerm_postgresql_database" "app_db" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_server.postgres_server.name
+
+  charset   = "UTF8"
+  collation = "English_United States.1252"
+}
+
+# Create App Service Plan (Linux kind, using containers)
+resource "azurerm_app_service_plan" "asp" {
+  name                = "${var.app_name}-asp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  kind = "Linux"
+
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+
+  reserved = true # Required for Linux plans
+}
+
+# Web App for Containers - using the image from ACR
+resource "azurerm_web_app" "webapp" {
+  name                = "${var.app_name}-webapp"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.asp.id
+
+  site_config {
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/${var.app_name}:latest"
+
+    app_command_line = "" # optional, if container needs startup command
+
+    acr_use_managed_identity = false   # We'll use admin user instead
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"          = "https://${azurerm_container_registry.acr.login_server}"
+    "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr.admin_password
+
+    # Your application environment variables (example: database connection string)
+    # Construct the connection string for your app
+    "DATABASE_URL" = "postgresql://${var.db_username}:${var.db_password}@${azurerm_postgresql_server.postgres_server.fqdn}:5432/${azurerm_postgresql_database.app_db.name}?sslmode=require"
     
-    database_flags {
-      name  = "cloudsql.iam_authentication"
-      value = "on"
-    }
-    
-    backup_configuration {
-      enabled = true
-    }
-    
-    ip_configuration {
-      ipv4_enabled    = true
-      authorized_networks {
-        name  = "all"
-        value = "0.0.0.0/0"
-      }
-    }
+    # Additional env vars like PORT, NODE_ENV, Firebase keys etc can be added here:
+    # "PORT" = "8080"
+    # "NODE_ENV" = "production"
+    # "FIREBASE_PROJECT_ID" = ...
+    # "FIREBASE_CLIENT_EMAIL" = ...
+    # "FIREBASE_PRIVATE_KEY" = ...
+    # "FIREBASE_DATABASE_URL" = ...
   }
-  
-  deletion_protection = false
-  depends_on = [google_project_service.required_apis]
+
+  # Enable Always On to avoid cold starts
+  https_only                  = true
+  client_affinity_enabled     = false
+  enable_automatic_tls        = true
+
+  depends_on = [
+    azurerm_container_registry.acr,
+    azurerm_postgresql_server.postgres_server,
+    azurerm_postgresql_database.app_db,
+    azurerm_app_service_plan.asp
+  ]
 }
 
-# Create database
-resource "google_sql_database" "database" {
-  name     = var.app_name
-  instance = google_sql_database_instance.main.name
+# Output the Web App URL
+output "webapp_url" {
+  value = "https://${azurerm_web_app.webapp.default_site_hostname}"
+  description = "The public URL for the Azure Web App"
 }
 
-# Create database user
-resource "google_sql_user" "user" {
-  name     = "app_user"
-  instance = google_sql_database_instance.main.name
-  password = var.db_password
-}
-
-# Create Cloud Run service
-resource "google_cloud_run_service" "app" {
-  name     = var.app_name
-  location = var.region
-  
-  template {
-    spec {
-      containers {
-        image = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.app_repo.name}/${var.app_name}:latest"
-        
-        ports {
-          container_port = 8080
-        }
-        
-        env {
-          name  = "DATABASE_URL"
-          value = "postgresql://${google_sql_user.user.name}:${var.db_password}@${google_sql_database_instance.main.public_ip_address}:5432/${google_sql_database.database.name}"
-        }
-        
-        resources {
-          limits = {
-            cpu    = "1000m"
-            memory = "512Mi"
-          }
-        }
-      }
-    }
-    
-    metadata {
-      annotations = {
-        "autoscaling.knative.dev/maxScale" = "10"
-        "run.googleapis.com/cloudsql-instances" = google_sql_database_instance.main.connection_name
-      }
-    }
-  }
-  
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-  
-  depends_on = [google_project_service.required_apis]
-}
-
-# Make Cloud Run service publicly accessible
-resource "google_cloud_run_service_iam_policy" "noauth" {
-  location = google_cloud_run_service.app.location
-  project  = google_cloud_run_service.app.project
-  service  = google_cloud_run_service.app.name
-
-  policy_data = data.google_iam_policy.noauth.policy_data
-}
-
-data "google_iam_policy" "noauth" {
-  binding {
-    role = "roles/run.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
+# Output the PostgreSQL server fully qualified domain name
+output "postgresql_fqdn" {
+  value       = azurerm_postgresql_server.postgres_server.fqdn
+  description = "PostgreSQL Server FQDN"
 }
